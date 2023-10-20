9c0a911026ac0e957203006989a84cb7849cc95b
Abdullahalnabhan <abedull12h@gmail.com>
2023-10-20 03:39:25 +0300

    complet task 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    alert('me name is abduallah  id number : 136517');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My React App</h1>
+        <button onClick={handleClick}>الاسود</button>
       </header>
     </div>
   );
